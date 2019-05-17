4576ee0c8393e9166fd9c3583ebb835c14d398ed
Aung Kaung Myat <akm97akm@gmail.com>
2019-05-17 15:59:59 +0800

    created

---
+++ LeaveApplication/src/main/java/com/iss/team1/LeaveApplication/LeaveApplication.java
@@ -0,0 +1,13 @@
+package com.iss.team1.LeaveApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveApplication.class, args);
+	}
+
+}
---
+++ LeaveApplication/src/test/java/com/iss/team1/LeaveApplication/LeaveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iss.team1.LeaveApplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeaveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
