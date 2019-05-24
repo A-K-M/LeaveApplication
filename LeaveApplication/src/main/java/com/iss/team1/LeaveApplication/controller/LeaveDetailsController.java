266697f2aad20d8e9eb80df4d11ebca9a024cac8
ayethinzarkyaw <athinzarkyaw@gmail.com>
2019-05-24 11:19:06 +0800

    add list in leavedetailController

--- LeaveApplication/src/main/java/com/iss/team1/LeaveApplication/controller/LeaveDetailsController.java
+++ LeaveApplication/src/main/java/com/iss/team1/LeaveApplication/controller/LeaveDetailsController.java
@@ -1,5 +1,47 @@
 package com.iss.team1.LeaveApplication.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.iss.team1.LeaveApplication.model.LeaveHistory;
+import com.iss.team1.LeaveApplication.repo.LeaveDetailsRepository;
+import com.iss.team1.LeaveApplication.repo.LeaveTypeRepository;
+import com.iss.team1.LeaveApplication.repo.StaffRepository;
+
+
+@Controller
 public class LeaveDetailsController {
 
+	private static final String leaveList = "leavelist";
+	private static final String leaveDetails = "leavedetails";
+	
+	private LeaveDetailsRepository ldRepo;
+	private LeaveTypeRepository ltRepo;
+	private StaffRepository sRepo;
+	
+	@Autowired
+	public void setldRepo(LeaveDetailsRepository ldRepo) {
+		this.ldRepo=ldRepo;
+	}
+	@Autowired
+	public void setltRepo(LeaveTypeRepository ltRepo) {
+		this.ltRepo=ltRepo;
+	}
+	@Autowired
+	public void setsRepo(StaffRepository sRepo) {
+		this.sRepo=sRepo;
+	}
+	
+	@RequestMapping(path="/leavelist")
+	public String listMethod(Model model) {
+		List<LeaveHistory> leaveHistories=ldRepo.findAll();
+		model.addAttribute("leaves", leaveHistories);
+		
+		System.out.println("saved");
+		return "list";
+	}
 }
